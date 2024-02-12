eeb2a618b603f83cbe29a7de3b9dfb1fc19627a3
LeonChief1 <doomaadon@gmail.com>
2024-02-12 14:52:07 +0300

    Добавление домашних заданий.

---
+++ lesson_1_task_8.py
@@ -0,0 +1,20 @@
+# Создайте функцию, которая принимает на вход параметр num и печатает этот параметр в консоль.
+
+num = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+def print_greeting1(num):
+    print (num)
+
+print_greeting1(8)
+print_greeting1(8)
+print_greeting1(0)
+print_greeting1(0)
+print_greeting1(5)
+print_greeting1(5)
+print_greeting1(5)
+print_greeting1(3)
+print_greeting1(5)
+print_greeting1(3)
+print_greeting1(5)
+
+print_greeting1(88005553535)
