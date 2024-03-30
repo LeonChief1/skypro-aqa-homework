c3696f5d7454f8f242dbc556f4ae25d878a77dc0
LeonChief1 <doomaadon@gmail.com>
2024-03-30 20:36:38 +0300

    дополнение информации

--- Lesson_1/lesson_1_task_2.py
+++ Lesson_1/lesson_1_task_2.py
@@ -1,3 +1,9 @@
+# my_age = 24
+# my_age = 27
+# print (my_age)
+
+# Или
+
 my_age = 24
-my_age = 27
+my_age = my_age + 3
 print (my_age)
--- Lesson_1/lesson_1_task_7.py
+++ Lesson_1/lesson_1_task_7.py
@@ -10,43 +10,50 @@
 #     9. Вывести в консоль 9
 #     10. Вывести в консоль 0
 
-def print_greeting1():
-    print ("Вывести в консоль 1")
+# def print_greeting1():
+#     print ("Вывести в консоль 1")
 
-def print_greeting2():
-    print ("Вывести в консоль 2")
+# def print_greeting2():
+#     print ("Вывести в консоль 2")
 
-def print_greeting3():
-    print ("Вывести в консоль 3")
+# def print_greeting3():
+#     print ("Вывести в консоль 3")
 
-def print_greeting4():
-    print ("Вывести в консоль 4")
+# def print_greeting4():
+#     print ("Вывести в консоль 4")
 
-def print_greeting5():
-    print ("Вывести в консоль 5")
+# def print_greeting5():
+#     print ("Вывести в консоль 5")
 
-def print_greeting6():
-    print ("Вывести в консоль 6")
+# def print_greeting6():
+#     print ("Вывести в консоль 6")
 
-def print_greeting7():
-    print ("Вывести в консоль 7")
+# def print_greeting7():
+#     print ("Вывести в консоль 7")
 
-def print_greeting8():
-    print ("Вывести в консоль 8")
+# def print_greeting8():
+#     print ("Вывести в консоль 8")
 
-def print_greeting0():
-    print ("Вывести в консоль 0")
+# def print_greeting0():
+#     print ("Вывести в консоль 0")
 
 # Вызовите объявленные функции в таком порядке, чтобы на экране отобразился номер 88005553535.
 
-print_greeting8()
-print_greeting8()
-print_greeting0()
-print_greeting0()
-print_greeting5()
-print_greeting5()
-print_greeting5()
-print_greeting3()
-print_greeting5()
-print_greeting3()
-print_greeting5()
+# print_greeting8()
+# print_greeting8()
+# print_greeting0()
+# print_greeting0()
+# print_greeting5()
+# print_greeting5()
+# print_greeting5()
+# print_greeting3()
+# print_greeting5()
+# print_greeting3()
+# print_greeting5()
+
+# Или
+
+def print_1():
+  print(88005553535, end='') 
+
+print_1()
