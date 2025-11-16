397a80b0b5da33c6a25a1752b3477acfa1c3d796
LeonChief1 <158843285+LeonChief1@users.noreply.github.com>
2025-11-16 22:01:57 +0300

    Update ResultPage.py

--- Lesson_8/homework/homework_2/pages/ResultPage.py
+++ Lesson_8/homework/homework_2/pages/ResultPage.py
@@ -9,6 +9,8 @@ class ResultPage:
     def result_answer(self,number):
         WebDriverWait(self._driver, 46, 0.1).until(EC.text_to_be_present_in_element( (By.CSS_SELECTOR, ".screen"), number))
 
-        result_element = self._driver.find_element(By.CSS_SELECTOR, ".screen")
+        result_element = self._driver.find_element(By.CSS_SELECTOR, ".screen").text
 
-        assert result_element.text == number, f"Получен результат {result_element.text}"
+        return result_element
+
+        # assert result_element == number, f"Получен результат {result_element}"
